541d433359e854b7aa55cc678dd31ecbe0009054
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-09-15 19:38:24 -0300

    feat: criando repositories para novas entidades

--- src/main/java/br/com/cpcl/repository/RoleRepository.java
+++ src/main/java/br/com/cpcl/repository/RoleRepository.java
@@ -3,9 +3,9 @@ package br.com.cpcl.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.cpcl.entity.Comercio;
+import br.com.cpcl.entity.Role;
 
 @Repository
-public interface ComercioRepository extends JpaRepository<Comercio, Long>{
+public interface RoleRepository extends JpaRepository<Role,Long>{
     
 }
--- src/main/java/br/com/cpcl/repository/UsuarioRepository.java
+++ src/main/java/br/com/cpcl/repository/UsuarioRepository.java
@@ -1,11 +1,13 @@
 package br.com.cpcl.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.cpcl.entity.Comercio;
+import br.com.cpcl.entity.Usuario;
 
 @Repository
-public interface ComercioRepository extends JpaRepository<Comercio, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
     
 }
