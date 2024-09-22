b0b84fcff4e4fa12f1ab16f75fa69cb189a88a7f
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-09-22 16:48:51 -0300

    feat: adicionando metodo jpql

--- src/main/java/br/com/cpcl/repository/ProdutoRepository.java
+++ src/main/java/br/com/cpcl/repository/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package br.com.cpcl.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.cpcl.entity.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+    List<Produto> findAllByComercioId(Long comercioId);
     
 }
