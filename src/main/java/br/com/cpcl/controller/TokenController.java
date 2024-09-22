23301d6b43674784ee584fa1b5f3cb4664b08689
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-09-21 21:05:03 -0300

    feat: criando controller para gerar tokens

---
+++ src/main/java/br/com/cpcl/controller/TokenController.java
@@ -0,0 +1,61 @@
+package br.com.cpcl.controller;
+
+import java.time.Instant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cpcl.dto.LoginRequest;
+import br.com.cpcl.dto.LoginResponse;
+import br.com.cpcl.repository.UsuarioRepository;
+
+@RestController
+public class TokenController {
+    
+    private final JwtEncoder jwtEncoder;
+    private final UsuarioRepository usuarioRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder, UsuarioRepository usuarioRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.usuarioRepository = usuarioRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+
+        var usuario = usuarioRepository.findByEmail(loginRequest.email());
+
+        System.out.println("chegou a");
+
+        if(usuario.isEmpty() || !usuario.get().isLoginCorreto(loginRequest, bCryptPasswordEncoder)){
+            throw new BadCredentialsException("Usuario ou senha inválido!");
+        }
+        System.out.println("chegou");
+
+        var now = Instant.now();
+        var tempoExpiracao = 604800L;
+
+        var claims = JwtClaimsSet.builder()
+            .issuer("cpcl")
+            .subject(usuario.get().getEmail())
+            .issuedAt(now)
+            .expiresAt(now.plusSeconds(tempoExpiracao))
+            .build();
+
+        var token = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponse(token, tempoExpiracao));
+
+    }
+
+}
